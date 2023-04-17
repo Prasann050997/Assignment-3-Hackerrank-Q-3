2181d19746f26ef054887a6e78c4415ec2406f5b
Patil Prasann <124381560+Prasann050997@users.noreply.github.com>
2023-04-17 22:49:59 +0530

    Add files via upload

---
+++ Assignment-3-Hackerrank-Q-3.py
@@ -0,0 +1,11 @@
+# itertools.permutations() in Python - Hacker Rank Solution
+# itertools.permutations() in Python - Hacker Rank Solution START
+from itertools import permutations
+
+s,k = input().split()
+
+words = list(permutations(s,int(k)))
+words = sorted(words, reverse=False)
+for word in words:
+    print(*word,sep='')
+# itertools.permutations() in Python - Hacker Rank Solution END
